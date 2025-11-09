4b345a82a365c213b968b0a2dfabadac4b9f126d
maryblessing-tech <isolokwu.obiagelimaryblessing@gmail.com>
2025-11-09 01:26:53 +0100

    Fix exercise 7: no output for negative numbers

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 
-if (isNaN(x) || x <= 0) {
+if (args[0] === undefined) {
     console.log("Missing number of occurrences");
-} else {
+} else if (x > 0) {
     let i = 0;
     while (i < x) {
         console.log("C is fun");
